43ecb62e75646739007c38e68b69d3d817635486
Yacine Ben Mhenni <yassoyacine0009@gmail.com>
2023-10-08 15:46:30 +0100

    declarative method

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,14 @@ export default function App() {
 
   function toggle(id) {
     setSquares((prevSquares) => {
-      const newSquares = [];
-      for (let i = 0; i < prevSquares.length; i++) {
-        const currentSquare = prevSquares[i];
-        if (currentSquare.id === id) {
-          const updatedSquare = {
-            ...currentSquare,
-            on: !currentSquare.on,
-          };
-          newSquares.push(updatedSquare);
-        } else {
-          newSquares.push(currentSquare);
-        }
-      }
-      return newSquares;
+      return prevSquares.map((square) => {
+        return square.id === id ? { ...square, on: !square.on } : square;
+      });
     });
   }
 
   const squareElements = squares.map((square) => (
-    <Box key={square.id} id={square.id} on={square.on} toggle={toggle} />
+    <Box key={square.id} on={square.on} toggle={() => toggle(square.id)} />
   ));
 
   return <main>{squareElements}</main>;
