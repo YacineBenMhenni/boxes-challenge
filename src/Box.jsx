c80a55d403f029ef11e2ea5ff4b40898ee98b393
Yacine Ben Mhenni <yassoyacine0009@gmail.com>
2023-10-08 11:38:23 +0100

    Local state

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 export default function Box(props) {
+  const [on, setOn] = React.useState(props.on);
+
   const styles = {
-    backgroundColor: props.on ? "#222222" : "none",
+    backgroundColor: on ? "#222222" : "transparent",
   };
 
-  return <div style={styles} className="box"></div>;
+  function toggle() {
+    setOn((prevOn) => !prevOn);
+  }
+
+  return <div style={styles} className="box" onClick={toggle}></div>;
 }
